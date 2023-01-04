0acfe1637c76d9a8ab41112593676f73c1023abd
JavierKamelo <jaecamelome@unal.edu.co>
2023-01-03 22:48:45 -0500

    adiciona modulo graficador

--- simulacionRuido_H.cpp
+++ simulacionRuido_H.cpp
@@ -47,6 +47,9 @@ void Ruido::Cargue(void){
     }
 
 }
+//----------------------------------------------------------------------------------------------------------------
+//  Crea Achivos .dat con los datos a graficar  
+//-----------------------------------------------------------------------------------------------------------------
 
 void Ruido::Imprimase(const char * NombreArchivo){
   ofstream MiArchivo(NombreArchivo); 
@@ -65,40 +68,74 @@ void Ruido::Imprimase(const char * NombreArchivo){
 
   MiArchivo.close();
 }
-
-void convIntEnChar(int entero, char cadenaTemp [4])
+//*********************************************************************************************************************
+// Convierte entero en una cadena de 4 números, aquí se usa para etiquetar secuencia de archivos desde 0000 hasta 9999 
+//**********************************************************************************************************************
+void convIntEnChar(int entero, char cadenaTemp [5])
 {
   int div, resi;
-  char k = '0';  
-  div = entero/100;
-  resi= entero%100;
+  char k = '0';
+  div = entero/1000;
+  resi= entero%1000;
   k = div + 0x30;
-  cadenaTemp[0] = k;
+  cadenaTemp[0] = k;  
+  div = resi/100;
+  resi= resi%100;
+  k = div + 0x30;
+  cadenaTemp[1] = k;
   div = resi/10;
   resi = resi%10;
   k = div + 0x30;
-  cadenaTemp[1] = k;
-  k = resi + 0x30;
   cadenaTemp[2] = k;
-  cadenaTemp[3]='\0'; 
+  k = resi + 0x30;
+  cadenaTemp[3] = k;
+  cadenaTemp[4]='\0'; 
    
+}
+//**********************************************************************************************************************
+void graficar(char ordenCuadro[5])
+{
+  char plotIni[100]="splot 'datos_";
+  char plotParam[70]=" using 1:2:3:4 with points pt 7 ps 0.4 lc palette";
+  char plotExt[6]=".dat'";
+  char datosIni[32]="set output 'cuadro_";
+  char datoExt[6]=".png'";
+  strcat(plotIni,ordenCuadro);
+  strcat(plotIni,plotExt);
+  strcat(plotIni,plotParam);
+  strcat(datosIni,ordenCuadro);
+  strcat(datosIni,datoExt); 
+  cout<<"set terminal png"<<endl;
+  cout<< datosIni <<endl;
+  cout<<"set vgrid $v size 100"<<endl;
+  cout<<"set vxrange [0:100]; set vyrange [0:100]; set vzrange [0:100]"<<endl ;
+  cout<<"set xrange [0:100]; set yrange [0:100]; set zrange [0:100]" <<endl;
+  cout<<"set pm3d"<<endl;
+  cout<<"set cbrange [-1.2:1.3]"<<endl;
+  cout<<"set ylabel \"y\""<<endl;
+  cout<<"set xlabel \"x\""<<endl;
+  cout<<"set zlabel \"z\""<<endl;
+  cout<<"set title \"Ruido Dependiente de H [0,2] \""<<endl;
+  cout<<"set key noautotitle"<<endl;
+  cout<<plotIni<<endl;
+
 }
 
 int main(void){
-  Ruido Fondo;
+Ruido Fondo;
 int m=0;
-char orden [4];
-cout << orden << endl;
+char orden [5];
+char extArchivo[5]=".dat";
 
   for ( H=0;H<Hmax; H=m*dH)
   {
+    char nomArchivo[14]="datos_";
     Fondo.Cargue();
     convIntEnChar(m,orden);
-    char nomArchivo[5]="y_";
     strcat(nomArchivo,orden);
-    const char *arch = nomArchivo;
-    cout<< "H es: " << H << "  k = "<<nomArchivo << "  m="<< m << endl;
+    strcat(nomArchivo,extArchivo);
     Fondo.Imprimase(nomArchivo);
+    graficar(orden);
     m++;
   }
   return 0;
